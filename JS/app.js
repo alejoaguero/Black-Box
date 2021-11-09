3feb8a772035fbeab77e1cd356c94c31f75027ee
Alejo <alejoleonel45@gmail.com>
2021-11-08 23:51:44 -0300

    Uso de Jquery y cambio de aspecto del carrito

--- JS/app.js
+++ JS/app.js
@@ -1,86 +1,38 @@
-const contenedor = document.getElementById('productos')
-let sumaCarrito = document.createElement('div')
-let contador = document.getElementById('cont')
-const carro = document.getElementById('carro')
-let ventaEmergente = document.createElement('div')
+$(document).ready(function(){
 
-ventaEmergente.classList.add('ventanaFloat')
+    const productos =   [ 
+            {id: 1, img: 'Images/netbook.jfif', descripcion: "Netbook Hp 16 ", precio: 8000},
+            {id: 2, img: 'Images/smartv.jpg', descripcion: "Smartv 45 pg", precio: 45000},
+            {id: 3, img: 'Images/ps4.png', descripcion: "Playstation 4", precio: 60000},
+            {id: 4, img: 'Images/ps5.jpeg',descripcion: "Playstation 5", precio: 100000},
+            {id: 5, img: 'Images/mouse.png',descripcion: "Mouse Gaming X-G200", precio: 1000},
+            {id: 6, img: 'Images/impresoras.jfif',descripcion: "Impresora", precio: 19000},
+            {id: 7, img: 'Images/nintendo.png',descripcion: "Nintendo Switch", precio: 70000}
 
-// array de objetos
-const productos = [
+    ]
 
-    {id: 1, img: 'Images/netbook.jfif', descripcion: "Netbook Hp 16 ", precio: 8000},
-    {id: 2, img: 'Images/smartv.jpg', descripcion: "Smartv 45 pg", precio: 45000},
-    {id: 3, img: 'Images/ps4.png', descripcion: "Playstation 4", precio: 60000},
-    {id: 4, img: 'Images/ps5.jpeg',descripcion: "Playstation 5", precio: 100000},
-    {id: 5, img: 'Images/mouse.png',descripcion: "Mouse Gaming X-G200", precio: 1000},
-    {id: 6, img: 'Images/impresoras.jfif',descripcion: "Impresora", precio: 19000},
-    {id: 7, img: 'Images/nintendo.png',descripcion: "Nintendo Switch", precio: 70000}
-];
-let i=1;   
-for (const producto of productos) {
-    let contend = document.createElement('div')
-    let descrip = document.createElement('p')
-    let img = document.createElement('img')
-    let precio = document.createElement('h5')
-    let boton = document.createElement('button')
+    let i=0;
+    let precioTotal;
+        for (const producto of productos) {
+               
+            $('#productos').append(`<div class="card contenedores">
+                                        <img src="${producto.img}" class="card-img-top">
+                                        <h5 class="card-title">${producto.descripcion}</h5>
+                                        <p>${producto.precio}</p>
+                                        <button id="${producto.id}" class="captura">Agregar</button>
+                                    </div>`)
 
-            img.src = producto.img
-            descrip.textContent = producto.descripcion
-            precio.textContent = producto.precio
-            boton.textContent = 'Agregar'
-            boton.id = producto.id
+                        
+                        $(`#${producto.id}`).click( function capturarProdu(e) {     
+                            if(e.target){
+                                i++;
+                                $('#cont').text(i);
+                                        
+                                    precioTotal += producto.precio;
+                                }
+                                    
+                        })
+                    }              
 
-                contend.append(img)
-                img.classList.add('card-img-top')
-                contend.append(descrip)
-                descrip.classList.add('card-title')
-                contend.append(precio)
-                precio.classList.add('card-text')
-                contend.append(boton)
-                contend.classList.add('contenedores')
-                contend.classList.add('card')
-                contend.style.width = '14rem'
-                
-
-        contenedor.appendChild(contend)
-        
-            boton.addEventListener('click', function captura(e){
-             let textProduct = document.createElement('p')
-                contador.innerHTML = i++
-                    textProduct.innerHTML = producto.descripcion +" " +  'Precio: '+ producto.precio
-                        if(e.target){
-
-                            ventaEmergente.append(textProduct)
-                        }
-            })
             
-            carro.addEventListener('click',function mostrarCarrito(){
-                        contenedor.appendChild(ventaEmergente);     
-            })
-}
-
-class Animal{
-    constructor(tipos,genero){
-        this.tipos = tipos
-        this.genero = genero
-    }
-}
-
-
-class Perro extends Animal{
-    constructor(tipos,genero,pelaje){
-        super(tipos,genero)
-        this.pelaje = pelaje
-
-    }
-    ladrar(){
-        console.log('Guau Guau')
-    }
-}
-
-const perritoMalvado = new Perro('Perro pila','Macho','Calvo')
-
-
-console.log(perritoMalvado)     
-perritoMalvado.ladrar()
+});
