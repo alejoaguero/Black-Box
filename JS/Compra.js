aee520601373a5bfbe97107139e8ffcc9abc224a
Alejo <alejoleonel45@gmail.com>
2021-11-24 23:56:27 -0300

    Cambio en el contador del carrito y creacion de la clases para el carrito

---
+++ JS/Compra.js
@@ -0,0 +1,9 @@
+export class Compra{
+    constructor(){
+        this.producto = 0;
+        this.precio = 0;
+        this.cantidad = 0;
+    }
+
+
+}
--- JS/app.js
+++ JS/app.js
@@ -1,49 +1,43 @@
+import {Compra} from "./Compra.js";
 $(document).ready(function(){
+const compra = new Compra()   
+// Inicializando para el Carrito
+let i;
+i = JSON.parse(localStorage.getItem('contador'))
+let contador;    
+// Consumiendo archivo de productos
+const URL = 'JSON/productos.json'
+    $.getJSON(URL,function(respuesta, estado){
 
-    const productos =   [ 
-            {id: 1, img: 'Images/netbook.jfif', descripcion: "Netbook Hp 16 ", precio: 8000},
-            {id: 2, img: 'Images/smartv.jpg', descripcion: "Smartv 45 pg", precio: 45000},
-            {id: 3, img: 'Images/ps4.png', descripcion: "Playstation 4", precio: 60000},
-            {id: 4, img: 'Images/ps5.jpeg',descripcion: "Playstation 5", precio: 100000},
-            {id: 5, img: 'Images/mouse.png',descripcion: "Mouse Gaming X-G200", precio: 1000},
-            {id: 6, img: 'Images/impresoras.jfif',descripcion: "Impresora", precio: 19000},
-            {id: 7, img: 'Images/nintendo.png',descripcion: "Nintendo Switch", precio: 70000}
+        if(estado === "success"){
+            let productos = respuesta;
+        // Incluyendo la los datos en las cajas
+              for(const producto of productos) {
+                            $('#productos').append(`<div class="contenedores card">
+                                                        <img src="${producto.img}" class="card-img-top">
+                                                        <h5 class="card-title">${producto.descripcion}</h5>
+                                                        <p class="card-text bordes">$${producto.precio}</p>
+                                                        <button class="btn btn-primary boton" id="${producto.id}">Agregar</button>
+                                                    </div>`)
 
-    ]
-    let productosCarrito = [];
-    let precioTotal=0;
-        for (const producto of productos) {
-            let i;    
-                if(localStorage.getItem('cantidad') == i){
-                    i=0
-                     }
-                        else
-                            i = localStorage.getItem('cantidad')       
-            $('#productos').append(`<div class="card contenedores">
-                                        <img src="${producto.img}" class="card-img-top">
-                                        <h5 class="card-title">${producto.descripcion}</h5>
-                                        <p>${producto.precio}</p>
-                                        <button id="${producto.id}" class="captura btn btn-primary">Agregar</button>
-                                    </div>`)
-
-                        
-                        $(`#${producto.id}`).click( function capturarProdu(e){ 
-                            $('#carro').hide(500)
-                                        .show(500)
-                                        
-                                        if(e.target){
-                                            i++;
-                                            $('#cont').text(i);
-                                                precioTotal += producto.precio;
-                                                    productosCarrito.push(`<p>${producto.descripcion} / Precio: $${producto.precio}</p>`)
-                                                    JSON.stringfy(productosCarrito)
-                                        }                 
-                        })
-                                
-                            $('#carro').click(function ventaEmergente(){
-                                $('.pop__up').addClass('ventanaCarrito')
-                            })
-
-                    }
+                                        // Contador del Carrito 
+                                                        $(`#${producto.id}`).click((e)=>{    
+                                                            i++
+                                                                localStorage.setItem('contador',JSON.stringify(i))
+                                                                contador = localStorage.getItem('contador')
+                                                                    $('#cont').text(contador)
+                                                                            if(e.target == producto.id){
+                                                                                    compra.producto = producto.descripcion
+                                                                                    compra.precio += producto.precio
+                                                                                    compra.cantidad += 1 
+                                                                            }
+                                                                            console.log(compra.cantidad)
+                                                    })  
+                                                                    }
+            }                           
+            else
+                console.log('no funciono')
+    })
+    $('#cont').text(localStorage.getItem('contador'))
 
             })
