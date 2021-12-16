e873fd5f6c11a3d06a1237bead0bd324e2c2e1db
Alejo <alejoleonel45@gmail.com>
2021-12-16 02:52:22 -0300

    Creacion del carrito y reemplazo de los productos repetidos, guardados en el localStorage

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    let carro = {}
+    
     let inicio = localStorage.getItem('cant');
     let carroCant = document.querySelector('#contCarro').textContent = localStorage.getItem('cant')
         fetch("../JSON/destacados.json")
@@ -20,23 +22,76 @@ $(document).ready(function(){
 
                                                 let carroCant = document.querySelector('#contCarro').textContent = cons
 
-                                                    $('.menuCarrito').append(`<div class="cajaCarrito">
-                                                                                <img src="${element.imagen}" class="imgCarrito">
-                                                                                <p>${element.nombre}</p>
-                                                                                <h6 class="text-danger precioCarrito">$${element.precio}</h6>
-                                                                             </div>
-                                                                             <hr>
-                                                                                `)
-                                }
+                                                    const producto = {
+                                                        id: element.id,
+                                                        nombre: element.nombre,
+                                                        precio: element.precio,
+                                                        imagen: element.imagen,
+                                                        cantidad: 1
+                                                    }
+                                                            if(carro.hasOwnProperty(producto.id)){
+                                                                producto.cantidad  = carro[producto.id].cantidad + 1
+                                                            }                   
+                                                
+                                                                carro[producto.id] = {...producto}
+
+                                                                const productosCarro = document.querySelector('#productosCarro')
+                                                                productosCarro.innerHTML = " "
+
+
+                                                                    Object.values(carro).forEach((element)=>{
+                                                                                 let   produ   =  `<div class="cajaCarrito" id="${element.id}">
+                                                                                                        <img src="${element.imagen}" class="imgCarrito">
+                                                                                                        <p>${element.nombre}</p>
+                                                                                                        <h5 class="text-danger precioCarrito">$${element.precio}</h5>
+                                                                                                        <p>${element.cantidad}</p>
+                                                                                                    </div>
+                                                                                                    <hr>
+                                                                                                    `
+                                                                                          $('#productosCarro').append(produ)
+
+                                                                                        localStorage.setItem('items',JSON.stringify(carro))
+                                                                    })  
+                                    }
                                     else{
                                         console.log('no funciona')
                                     }                                        
                             })
 
+                                if(localStorage.getItem('items')){
+                                    carro = JSON.parse(localStorage.getItem('items'))
+                                    
+                                    productosCarro.innerHTML = " "
+
+                                    Object.values(carro).forEach((element)=>{
+                                        let   produ   =  `<div class="cajaCarrito" id="${element.id}">
+                                                               <img src="${element.imagen}" class="imgCarrito">
+                                                               <p>${element.nombre}</p>
+                                                               <h5 class="text-danger precioCarrito">$${element.precio}</h5>
+                                                               <p>${element.cantidad}</p>
+                                                           </div>
+                                                           <hr>
+                                                           `
+
+                                                           $('#productosCarro').append(produ)
+                                            })
+                            
+                                }
                 });
             })
 
+            
+                            
+            
             .catch(data=>{
                 console.log('error')
             })
+
+                $('.comprasCarro').click(()=>{
+                    $('.menuCarrito').css("display","block")
+                })
+
+                    $('#cerrarCarro').click(()=>{
+                        $('.menuCarrito').css("display","none")
+                    })
 })
